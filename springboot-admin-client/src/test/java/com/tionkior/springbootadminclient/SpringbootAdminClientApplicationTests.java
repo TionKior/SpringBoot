bdee09a06c17eea227009b2bea45e283771b8e56
TionKior <1423304936@qq.com>
2021-06-22 18:14:15 +0800

    feat: SpringBoot练习,部署

---
+++ springboot-admin-client/src/test/java/com/tionkior/springbootadminclient/SpringbootAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tionkior.springbootadminclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
